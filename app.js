34a87fe6933aac800cdd53ff2de351ef7cbe126d
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-07 17:26:12 +0400

    check status

--- app.js
+++ app.js
@@ -79,3 +79,20 @@ list.innerHTML += "<li>Platform: " + navigator.platform;
 if (window.addEventListener) {
   list.innerHTML += "<li>This is a modern DOM browser";
 }
+
+const info = document.getElementById("infoss");
+let status = "";
+status = navigator.javaEnabled() ? "Enabled" : "Disabled";
+info.innerHTML += "Java Support is " + status + "<hr>";
+status = navigator.cookieEnabled ? "Enabled" : "Disabled";
+info.innerHTML += "Cookie Support is " + status + "<hr>";
+if (navigator.plugins.length !== 0) {
+  info.innerHTML += "No. of Plugins: " + navigator.plugins.length;
+  info.innerHTML += "<br>Example: " + navigator.plugins[0].name;
+  info.innerHTML += "<br>For: " + navigator.plugins[0].description + "<hr>";
+}
+if (navigator.mimeTypes.length !== 0) {
+  info.innerHTML += "No. of MIME Types: " + navigator.mimeTypes.length;
+  info.innerHTML += "<br>Example: " + navigator.mimeTypes[1].type;
+  info.innerHTML += "<br>For: " + navigator.mimeTypes[1].description;
+}
